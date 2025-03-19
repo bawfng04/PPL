30e3d3f7f42ac1d28b4e115340fdaa3b7d3b0162
bawfng04 <nguyendinhbang53az@gmail.com>
2025-03-19 14:10:16 +0700

    error test 8

--- BTL/BTL3/MiniGO_BTL3_TASK1/StaticCheck.py
+++ BTL/BTL3/MiniGO_BTL3_TASK1/StaticCheck.py
@@ -44,44 +44,104 @@ class StaticChecker(BaseVisitor,Utils):
     def visitProgram(self, ast: Program, c: List[List[Symbol]]):
         # TODO: Global scope
 
+        for decl in ast.decl:
+            self.visit(decl, c)
+        return c
+
         # TODO: Function/method scope and VarDecl => self.function_current, self.struct_current
 
         return c
 
     def visitParamDecl(self, ast: ParamDecl, c: list[Symbol]) -> Symbol:
         # TODO Redeclared ParamDecl
+        # kiểm tra redeclare parameter
+        #
+        for param in c: #duyệt qua các parameter
+            if param.name == ast.parName: #nếu tên parameter đã tồn tại
+                raise Redeclared(Parameter(), ast.parName)
+
         return Symbol(ast.parName, None, None)
 
     def visitVarDecl(self, ast: VarDecl, c : List[List[Symbol]]) -> Symbol:
         # TODO Redeclared Variable
+        # res là kết quả tìm kiếm biến trong stack
         res = self.lookup(ast.varName, c[0], lambda x: x.name)
-        if not res is None:
+        if not res is None: #nếu đã tồn tại thì báo lỗi
             raise Redeclared(Variable(), ast.varName)
+        # tạo một variable symbol mới
+        symbol = Symbol(ast.varName, ast.varType, None)
+        # thêm symbol vào stack
+        c[0].insert(0, symbol)
+        return symbol
         return Symbol(ast.varName, None, None)
 
     def visitConstDecl(self, ast: ConstDecl, c : List[List[Symbol]]) -> Symbol:
         # TODO Redeclared Constant
+        res = self.lookup(ast.conName, c[0], lambda x: x.name)
+        if not res is None:
+            raise Redeclared(Constant(), ast.conName)
+        # tạo một constant symbol mới
+        symbol = Symbol(ast.conName, ast.conType, ast.iniExpr)
+        # thêm symbol vào stack
+        c[0].insert(0, symbol)
+        return symbol
         return Symbol(ast.conName, None, None)
 
     def visitFuncDecl(self, ast: FuncDecl, c : List[List[Symbol]]) -> Symbol:
         # TODO Redeclared Function
+        res = self.lookup(ast.name, c[0], lambda x: x.name)
+        if not res is None:
+            raise Redeclared(Function(), ast.name)
+        # tạo một function symbol mới
+        mtype = MType([param.parType for param in ast.params], ast.retType)
+        symbol = Symbol(ast.name, mtype, None)
+        # thêm symbol vào stack
+        c[0].insert(0, symbol)
+        # tạo scope mới cho function
+        param_scope = []
+        for param in ast.params:
+            param_sym = self.visitParamDecl(param, param_scope)
+            param_scope.append(param_sym)
+        # visit function body với scope mới
+        self.visit(ast.body, [[]] + c)
+        return symbol
+
         return Symbol(ast.name, None, None)
 
     def visitStructType(self, ast: StructType, c) -> StructType:
          # TODO Redeclared Field
+
+        # kiểm tra redeclare field
+        field_names = []
+        for field_name, field_type in ast.elements:
+            if field_name in field_names:
+                raise Redeclared(Field(), field_name)
+            field_names.append(field_name)
+        # thêm struct vào stack
+        self.struct.append(ast)
+
         return ast
 
     def visitMethodDecl(self, ast: MethodDecl, c: List[MethodDecl]) -> MethodDecl:
         # TODO Redeclared Method
+        for method in c:
+            if method.fun.name == ast.fun.name:
+                raise Redeclared(Method(), ast.fun.name)
+        # thêm method vào stack
+        c.append(ast)
         return ast
 
     def visitPrototype(self, ast: Prototype, c: List[Prototype]) -> Prototype:
         # TODO Redeclared Prototype
+        for proto in c:
+            if proto.name == ast.name:
+                raise Redeclared(Prototype(), ast.name)
+        # thêm prototype vào stack
+        c.append(ast)
         return ast
 
     def visitInterfaceType(self, ast: InterfaceType, c) -> InterfaceType:
         reduce(lambda acc,ele: [self.visit(ele,acc)] + acc , ast.methods , [])
-        return ast
 
     def visitForBasic(self, ast: ForBasic, c : List[List[Symbol]]) -> None:
         # TODO
