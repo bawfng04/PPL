dad6a86018a757a42bede8bce3abd1d56729db5f
bawfng04 <nguyendinhbang53az@gmail.com>
2025-03-27 22:19:48 +0700

    update before changes

--- BTL/BTL3/MiniGO_BTL3_TASK4/CheckSuite.py
+++ BTL/BTL3/MiniGO_BTL3_TASK4/CheckSuite.py
@@ -544,3 +544,27 @@ type TIEN interface {VoTien ();}
                          VarDecl("c", BoolType(), BinaryOp("==", StringLiteral("1"), StringLiteral("2"))),
                          VarDecl("d", BoolType(), BinaryOp(">", IntLiteral(1), FloatLiteral(2.0)))])
         self.assertTrue(TestChecker.test(input, "Type Mismatch: BinaryOp(IntLiteral(1),>,FloatLiteral(2.0))", inspect.stack()[0].function))
+
+
+    def test_052(self):
+        """
+        func foo(){
+            for var i int = 1; a < 10; i := 1. {
+                var a = 1;
+            }
+        }
+        """
+        input = Program([FuncDecl("foo",[],VoidType(),Block([ForStep(VarDecl("i",IntType(),IntLiteral(1)),BinaryOp("<", Id("a"), IntLiteral(10)),Assign(Id("i"),FloatLiteral(1.)),Block([VarDecl("a", None,IntLiteral(1))]))]))])
+        self.assertTrue(TestChecker.test(input, "Type Mismatch: VarDecl(i,IntType,FloatLiteral(1.0))", inspect.stack()[0].function))
+
+    def test_053(self):
+        """
+        func foo(){
+            for var i int = 1; i < 10; i := 1. {
+                return;
+            }
+        }
+        """
+        input = Program([FuncDecl("foo",[],VoidType(),Block([ForStep(VarDecl("i",IntType(),IntLiteral(1)),BinaryOp("<", Id("i"), IntLiteral(10)),Assign(Id("i"),FloatLiteral(1.)),Block([Return(None)]))]))])
+        self.assertTrue(TestChecker.test(input, "Type Mismatch: Assign(Id(i),FloatLiteral(1.0))", inspect.stack()[0].function))
+
